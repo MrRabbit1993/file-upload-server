df23b6b3ec07b9d59a3e7c3d7f85636bb5e31cb9
兔子先生 <1993iweb@gmail.com>
2020-10-21 17:33:37 +0800

    feat: 加入post请求解析

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const Koa = require('koa');
 
+const parser = require("koa-bodyparser");//解决post请求的参数
 
 const InitManager = require("./core/init");
 
 
 const app = new Koa();
-
+app.use(parser())
 
 InitManager.initCore(app);
 
